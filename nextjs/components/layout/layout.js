5324636825967aa46a162fb6df68cecc7b771782
kustomApe <kustomape@gmail.com>
2023-10-23 22:11:40 +0900

    Blog sample drf and nextjs structure.

--- nextjs/components/layout/layout.js
+++ nextjs/components/layout/layout.js
@@ -7,7 +7,7 @@ export default function Layout(props) {
             <main className='container flex flex-1 justify-center mx-auto px-5 max-w-screen-lg'>
                 {props.children}
             </main>
-            <footer className='flex items-center justify-center w-full h-20 text-sm border-t'>
+            <footer className='flex items-center justify-center w-full h-20 text-sm'>
                 Reserved.
             </footer>
         </div>
--- nextjs/pages/index.js
+++ nextjs/pages/index.js
@@ -1,13 +1,43 @@
+import Image from 'next/image';
 import Head from 'next/head';
+import Post from '../components/post/post';
+import { getAllPostsData } from '../lib/posts';
 
-export default function Home() {
+export default function Home({ posts }) {
   return (
     <div>
       <Head>
         <title>Kustom</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1 className="text-9xl flex justify-center h-screen items-center text-indigo-500">Kustom</h1>
+      <div className='mb-10 p-3 border rounded'>
+        <Image className='object-cover rounded'
+        src='/top.jpg'
+        alt='top'
+        width={1920}
+        height={1278}
+        />
+      </div>
+      <div className='flex justify-center flex-col items-center mb-10'>
+        <div className='text-lg mb-3'>
+          BLOG POSTS
+        </div>
+        <div className='border w-14'></div>
+      </div>
+      <div className='flex flex-wrap -m-4 mb-5'>
+        {
+          posts && posts.map((post) => <Post key={post.id} post={post} />)
+        }
+      </div>
+      {/* <h1 className="text-9xl flex justify-center h-screen items-center text-indigo-500">Kustom</h1> */}
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const posts = await getAllPostsData();
+  return {
+    props: { posts },
+    revalidate: 3,
+  };
 }
